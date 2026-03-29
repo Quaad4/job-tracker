3088cc0075f9dfa7ecde6170adfe0337d8893062
Quaad4 <ajwood1998@gmail.com>
2026-03-29 15:11:19 +0200

    add ApplicationsView and connect to Pinia store

---
+++ frontend/src/views/ApplicationsView.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>Job Tracker</h1>
+        <p v-if="store.loading">Loading...</p>
+        <ul v-else>
+            <li v-for="application in store.applications" 
+                :key="application.id">
+                {{ application.company }} — {{ application.role }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+
+import { onMounted } from 'vue';
+import { useApplicationStore } from '@/stores/applicationStore';
+
+const store = useApplicationStore()
+
+onMounted(() => {
+    store.fetchApplications()
+})
+
+</script>
