51540306cd545ce8c11009b1894d311f807758aa
Quaad4 <ajwood1998@gmail.com>
2026-03-31 23:27:41 +0200

    update getApplications to accept query params

--- frontend/src/api/applications.js
+++ frontend/src/api/applications.js
@@ -1,7 +1,7 @@
 import apiClient from './axios'
 
-export const getApplications = () => {
-    return apiClient.get('/applications')
+export const getApplications = (params = {}) => {
+    return apiClient.get('/applications', { params })
 }
 
 export const showApplication = (id) => {
