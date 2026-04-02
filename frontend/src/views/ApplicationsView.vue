9043821a27ab4be4f1c301ea2c5626168af4f2fc
Quaad4 <ajwood1998@gmail.com>
2026-04-02 17:59:03 +0200

    add applications view header

--- frontend/src/views/ApplicationsView.vue
+++ frontend/src/views/ApplicationsView.vue
@@ -1,14 +1,14 @@
 <template>
-    <div>
-        <h1>Job Tracker</h1>
-        <p v-if="store.loading">Loading...</p>
-        <ul v-else>
-            <li v-for="application in store.applications" 
-                :key="application.id">
-                {{ application.company }} — {{ application.role }}
-            </li>
-        </ul>
-    </div>
+    <div class="bg-blue-100 min-h-screen">        
+
+        <header class="bg-white shadow-sm">
+            <div class="max-w-7xl mx-auto px-4 py-5 flex justify-between items-center">
+                <h1 class="text-2x1 font-bold text-gray-800">🗂 Job Tracker</h1>
+                <button class="bg-blue-600 hover:bg-blue-709 text-white px-4 py-2 rounded-lg text-sm font-medium transition-colors cursor-pointer">+ Add Application</button>
+            </div>
+        </header>
+
+    </div>    
 </template>
 
 <script setup>
