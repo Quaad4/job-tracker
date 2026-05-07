2e66155ee12634750dd7686e5f0befdcdd71185c
Quaad4 <ajwood1998@gmail.com>
2026-05-07 19:08:51 +0200

    add sanctum to routes requiring auth

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,12 +4,15 @@ use App\Http\Controllers\ApplicationController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/applications', [ApplicationController::class, 'index']);
-Route::post('/applications', [ApplicationController::class, 'store']);
-Route::get('/applications/{application}', [ApplicationController::class, 'show']);
-Route::put('/applications/{application}', [ApplicationController::class, 'update']);
-Route::delete('/applications/{application}', [ApplicationController::class, 'destroy']);
-
 Route::post('/auth/register', [AuthController::class, 'register']);
 Route::post('/auth/login', [AuthController::class, 'login']);
-Route::post('/auth/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/auth/logout', [AuthController::class, 'logout']);
+
+    Route::get('/applications', [ApplicationController::class, 'index']);
+    Route::post('/applications', [ApplicationController::class, 'store']);
+    Route::get('/applications/{application}', [ApplicationController::class, 'show']);
+    Route::put('/applications/{application}', [ApplicationController::class, 'update']);
+    Route::delete('/applications/{application}', [ApplicationController::class, 'destroy']);
+});
