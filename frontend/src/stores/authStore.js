998f206fd11f819c1e8deed95830c971481b2c55
Quaad4 <ajwood1998@gmail.com>
2026-05-13 17:50:39 +0200

    add authStore to store user details, auth token and offer a login method

---
+++ frontend/src/stores/authStore.js
@@ -0,0 +1,32 @@
+import { defineStore } from 'pinia'
+import { ref, computed } from 'vue'
+import { authLogin } from '../api/auth'
+
+export const useAuthStore = defineStore('auth', () => {
+
+    // state
+    const user = ref(null)
+    const token = ref(localStorage.getItem('token') || null)
+
+    // computed
+    const isAuthenticated = computed(() => !!token.value)
+
+    const login = async (userData) => {
+        try {
+            const response = await authLogin(userData)
+            token.value = response.data.token
+            user.value = response.data.user
+            localStorage.setItem('token', token.value)
+        } catch (error) {
+            console.error('Login failed:', error)
+            throw error // LoginView can catch it
+        }
+    }
+
+    return {
+        user,
+        token,
+        isAuthenticated,
+        login
+    }
+})
