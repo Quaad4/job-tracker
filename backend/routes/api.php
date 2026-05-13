def9db7ee27f66b6444e4bc13c645dbc98a5e643
Quaad4 <ajwood1998@gmail.com>
2026-05-13 17:19:26 +0200

    make applications available to view for all users

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,11 +6,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/auth/register', [AuthController::class, 'register']);
 Route::post('/auth/login', [AuthController::class, 'login']);
+Route::get('/applications', [ApplicationController::class, 'index']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/auth/logout', [AuthController::class, 'logout']);
 
-    Route::get('/applications', [ApplicationController::class, 'index']);
     Route::post('/applications', [ApplicationController::class, 'store']);
     Route::get('/applications/{application}', [ApplicationController::class, 'show']);
     Route::put('/applications/{application}', [ApplicationController::class, 'update']);
