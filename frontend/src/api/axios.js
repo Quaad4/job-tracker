fe98330bbe64dd05a4f940614dbfca43810bf0a3
Quaad4 <ajwood1998@gmail.com>
2026-05-13 17:48:54 +0200

    add interceptor to add token if it exists in every request

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -9,4 +9,13 @@ const apiClient = axios.create({
     },
 })
 
+// Attach token to every request if it exists in localStorage
+apiClient.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token')
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+})
+
 export default apiClient
