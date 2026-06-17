e3bfbc3da48a5bf47c3b50a9416e9ed1feb10eb7
Quaad4 <ajwood1998@gmail.com>
2026-06-17 22:21:00 +0200

    update styling

--- frontend/src/views/ApplicationsView.vue
+++ frontend/src/views/ApplicationsView.vue
@@ -7,8 +7,10 @@
         <header class="bg-white shadow-sm">
             <div class="max-w-7xl mx-auto px-4 py-5 flex justify-between items-center">
                 <h1 class="text-2x1 font-bold text-gray-800">🗂 Job Tracker</h1>
-                <button @click="modal.openCreate()" class="bg-blue-600 hover:bg-blue-709 text-white px-4 py-2 rounded-lg text-sm font-medium transition-colors cursor-pointer">+ Add Application</button>
-                <button @click="authStore.logout()" class="bg-blue-600 hover:bg-blue-709 text-white px-4 py-2 rounded-lg text-sm font-medium transition-colors cursor-pointer">Logout</button>
+                <div class="justify-between items-center gap-2 flex">
+                    <button @click="modal.openCreate()" class="bg-blue-600 hover:bg-blue-709 text-white px-4 py-2 rounded-lg text-sm font-medium transition-colors cursor-pointer">+ Add Application</button>
+                    <button v-if="authStore.isAuthenticated" @click="authStore.logout()" class="bg-blue-600 hover:bg-blue-709 text-white px-4 py-2 rounded-lg text-sm font-medium transition-colors cursor-pointer">Logout</button>
+                </div>
             </div>
         </header>
 
